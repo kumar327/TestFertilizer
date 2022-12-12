f30a1604dd9b5a2170bdb84e01f5a21d894e0725
kumar327 <adigoppulakumar@gmail.com>
2022-12-13 00:24:43 +0530

    First Commit

---
+++ TestFertilizerServices/src/main/java/com/fertilizer/TestFertilizerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.fertilizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFertilizerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFertilizerServicesApplication.class, args);
+	}
+
+}
---
+++ TestFertilizerServices/src/test/java/com/fertilizer/TestFertilizerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fertilizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFertilizerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
